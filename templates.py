6c06f7f152dabc59adf7cb15d88bc0995d81a44b
Christer Karlsen <chris@ramosicked.com>
2023-02-06 22:24:16 +0000

    Initial commit from add.py

---
+++ templates.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Christer Karlsen
+# Email: chris@ramosicked.com
+# Project: templates
+# Copyright (c) 2023, Christer Karlsen
+# License: MIT License
+#
+"""
+Example script
+"""
